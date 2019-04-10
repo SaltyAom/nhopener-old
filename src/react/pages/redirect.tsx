ab60b56081d7a8b2f645c7b40d22b722d8c875f6
aomkirby123 <aomkirby123@gmail.com>
2019-04-10 14:36:48 +0700

    Add fallback for error

--- src/react/pages/redirect.tsx
+++ src/react/pages/redirect.tsx
@@ -5,11 +5,13 @@ import {
     ButtonBase,
     useState,
     useEffect,
-    Axios
+    Axios,
+    Link
 } from "../bridge"
 
 import "../../assets/css/redirect.css"
 import '../../assets/css/button.css'
+import '../../assets/css/error.css'
 
 const Error = Loadable({
     loader: () => import('./error'),
@@ -17,7 +19,7 @@ const Error = Loadable({
 });
 
 export default (props: any) => {
-    if(props.store.redirectURL !== undefined){    
+    if(props.store.redirectURL !== undefined){
         const [og, setOg]:any = useState("Fetching...");
 
         useEffect(() => {
@@ -25,34 +27,51 @@ export default (props: any) => {
 
             Axios(requestUrl).then((ogData:any) => {
                 setOg(ogData.data.data);
-            });
+            }).catch(() => {
+                setOg(undefined);
+            })
         },[]);
         
-        return (
-            <div id="pages">
-                <link rel="prefetch" href={`https://nhentai.net/g/${props.store.redirectURL}`} />
-                <div id="redirect-page">
-                    <div id="redirect-image-wrapper">
-                        { og.ogImage !== undefined ? 
-                            <img id="redirect-preview-image" src={og.ogImage.url} />
-                        : null }
-                    </div>
-                    <div id="redirect-detail">
-                        { og === "Fetching..." ? <h6 id="preview-fetching">Fetching...</h6>
-                        : <h6 id="redirect-code">{props.store.redirectURL}</h6> }
-                        <h2>{og.ogTitle}</h2>
-                        <h3>{og.ogDescription}</h3>
-                        { og === "Fetching..." ? null :
-                        <ButtonBase id="redirect-button">
-                            <a className="button secondary" href={`https://nhentai.net/g/${props.store.redirectURL}`} rel="noreferrer external nofollow">
-                                Read <i className="material-icons" style={{cursor:"pointer"}}>chevron_right</i>
-                            </a>
-                        </ButtonBase>
-                        }
+        if(og !== undefined && og !== "Fetching..."){
+            return (
+                <div id="pages">
+                    <link rel="prefetch" href={`https://nhentai.net/g/${props.store.redirectURL}`} />
+                    <div id="redirect-page">
+                        <div id="redirect-image-wrapper">
+                            { og.ogImage !== undefined ? 
+                                <img id="redirect-preview-image" src={og.ogImage.url} />
+                            : null }
+                        </div>
+                        <div id="redirect-detail">
+                            <h2>{og.ogTitle}</h2>
+                            <h3>{og.ogDescription}</h3>
+                            <h6 id="redirect-code">{props.store.redirectURL}</h6> 
+                            <ButtonBase id="redirect-button">
+                                <a className="button secondary" href={`https://nhentai.net/g/${props.store.redirectURL}`} rel="noreferrer external nofollow">
+                                    Read <i className="material-icons" style={{cursor:"pointer"}}>chevron_right</i>
+                                </a>
+                            </ButtonBase>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
+            )
+        } else if(og === "Fetching...") {
+            return (
+                <Loading />
+            )
+        } else {
+            return (
+                <div id="error">
+                    <h1>404</h1>
+                    <p>Stories not found...</p>
+                    <ButtonBase className="button-wrapper">
+                        <Link className="button" to="/generate">
+                            Return <i className="material-icons" style={{cursor:"pointer"}}>chevron_right</i>
+                        </Link>
+                    </ButtonBase>
+                </div>
+            )
+        }
     } else {
         return (
             <Error />
