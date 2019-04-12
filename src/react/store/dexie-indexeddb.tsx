11719bfe9884675a5443769f1bc4f4eb81e87c7c
aomkirby123 <aomkirby123@gmail.com>
2019-04-12 12:16:51 +0700

    Fixed settings not saving

--- src/react/store/dexie-indexeddb.tsx
+++ src/react/store/dexie-indexeddb.tsx
@@ -2,9 +2,18 @@ import Dexie from 'dexie'
 
 const openerIDB = new Dexie("opener");
 
-openerIDB.version(1).stores({
-    history: 'id++, title, timestamp',
-    settings: 'title, value'
+Dexie.exists("opener").then(async (exists:boolean) => {
+    await openerIDB.version(1).stores({
+        history: 'id++, title, timestamp',
+        settings: 'title, value'
+    });
+    if(!exists){
+        openerIDB.table("settings").put({
+            title: "blurDashboard",
+            vale: false
+        })
+    }
 });
+
 
 export default openerIDB
