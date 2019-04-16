8cea0650d57faba09ccdbaae4096b6cfb8b43973
aomkirby123 <aomkirby123@gmail.com>
2019-04-16 21:06:28 +0700

    Add SFW mode on warning

--- src/react/pages/warning.tsx
+++ src/react/pages/warning.tsx
@@ -1,5 +1,6 @@
 import React, {
     useContext,
+    useState,
     FunctionComponent,
     ReactElement
 } from 'react'
@@ -7,16 +8,31 @@ import {
     storeContext,
     ButtonBase,
     Link,
+    openerIDB
 } from '../bridge'
+import {
+    Checkbox
+} from "@material-ui/core"
 
 import '../../assets/css/button.css'
 import '../../assets/css/error.css'
 
 const Warning:FunctionComponent<any> = (props: any):ReactElement<any> => {
-    const dispatch:any = useContext(storeContext);
+    const dispatch:any = useContext(storeContext),
+        [blur, setBlur] = useState(false);
 
-    const allow = (e:any) => {
+    const allow = async(e:any) => {
         e.preventDefault();
+        if(blur === true){
+            await openerIDB.table("settings").put({
+                title: "blurDashboard",
+                value: true
+            });
+            await openerIDB.table("settings").put({
+                title: "blurPreview",
+                value: true
+            });
+        }
         dispatch({
             type: "setAllowance",
             allow: true
@@ -27,6 +43,16 @@ const Warning:FunctionComponent<any> = (props: any):ReactElement<any> => {
         <div id="warning">
             <h1>Are you legally classified as an adult?</h1>
             <p>You must be classified as an adult and legally allowed to view the contents of this site in the country you are currently located.</p>
+            <div id="warning-setting">
+                <Checkbox
+                    style={{transform: "scale(.925)"}}
+                    className="check"
+                    checked={blur}
+                    onChange={() => setBlur(!blur)}
+                    value="Blur every preview image (SFW mode)"
+                />
+                <label id="warning-label">Blur every preview image (SFW mode)</label>
+            </div>
             <ButtonBase className="button-wrapper" onClick={() => props.function()}>
                 <Link className="button" to="/" onClick={(e:any) => allow(e)}>
                     Proceed <i className="material-icons" style={{cursor:"pointer"}}>chevron_right</i>
