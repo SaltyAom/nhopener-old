48f6f767c2592da09305d48c1fdf618bb67d2ab4
aomkirby123 <aomkirby123@gmail.com>
2019-04-21 01:52:43 +0700

    Add reload button on loading screen stuck

--- src/react/component/loading.tsx
+++ src/react/component/loading.tsx
@@ -14,6 +14,7 @@ const Loading:FunctionComponent<any> = (props: any):ReactElement<any> => {
                 <svg className="circular circular-shadow" viewBox="25 25 50 50">
                     <circle className="path path-shadow" cx="50" cy="50" r="20" fill="none" strokeWidth="2" strokeMiterlimit="10"/>
                 </svg>
+                <a id="reload-button" onClick={() => document.location.reload()}>Reload?</a>
             </div>
         </div>
     )
@@ -26,6 +27,7 @@ const Loading:FunctionComponent<any> = (props: any):ReactElement<any> => {
                 <svg className="circular circular-shadow" viewBox="25 25 50 50">
                     <circle className="path path-shadow" cx="50" cy="50" r="20" fill="none" strokeWidth="2" strokeMiterlimit="10"/>
                 </svg>
+                <a id="reload-button" onClick={() => document.location.reload()}>Reload?</a>
             </div>
         </div>
     )
@@ -38,6 +40,7 @@ const Loading:FunctionComponent<any> = (props: any):ReactElement<any> => {
                 <svg className="circular circular-shadow" viewBox="25 25 50 50">
                     <circle className="path path-shadow" cx="50" cy="50" r="20" fill="none" strokeWidth="2" strokeMiterlimit="10"/>
                 </svg>
+                <a id="reload-button">Reload?</a>
             </div>
         </div>
     )
@@ -50,6 +53,7 @@ const Loading:FunctionComponent<any> = (props: any):ReactElement<any> => {
                 <svg className="circular circular-shadow" viewBox="25 25 50 50">
                     <circle className="path path-shadow" cx="50" cy="50" r="20" fill="none" strokeWidth="2" strokeMiterlimit="10"/>
                 </svg>
+                <a id="reload-button" onClick={() => document.location.reload()}>Reload?</a>
             </div>
         </div>
     )
