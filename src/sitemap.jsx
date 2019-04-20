895b8cd34f442700d9e6a79c2c9f9b384a073683
aomkirby123 <aomkirby123@gmail.com>
2019-04-20 21:56:59 +0700

    Add sitemap

---
+++ src/sitemap.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route } from 'react-router'
+
+export default (
+    <Route>
+            <Route path='/' />
+            <Route path='/drop' />
+            <Route path='/generate' />
+            <Route path='/history' />
+            <Route path='/' />
+            <Route path='/redirect' />
+            <Route path='/settings' />
+    </Route>
+)
