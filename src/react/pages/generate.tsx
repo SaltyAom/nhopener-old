276740dc35b576d4b70397075f75793975b85ff4
aomkirby123 <aomkirby123@gmail.com>
2019-05-13 16:51:04 +0700

    Fixed preview image not showing on generate page

--- src/react/pages/generate.tsx
+++ src/react/pages/generate.tsx
@@ -33,53 +33,7 @@ const Generate:FunctionComponent<any> = (props:props):ReactElement => {
     const [uri, setUri] = useState<string | any>("data:image/jpeg;base64,a"),
         [redirectState, setRedirectState]:any = useState<boolean | any>(false);
 
-    useEffect(() => {
-        dispatch({
-            type: "updateCode",
-            code: props.match.params.id
-        });
-    },[dispatch, props.match.params.id]);
-    useEffect(() => {
-        if(props.store.code === undefined) return;
-
-        let canvas:any = document.getElementById("generate-canvas"),
-            ctx:any = canvas.getContext("2d"),
-            color:string = props.store.code,
-            colorLength:number;
-        
-        if(color){
-            colorLength = color.length;
-        }
-
-        canvas.width = 256;
-        canvas.height = 256;
-
-        if(colorLength < 6){
-            for(let i=colorLength; i<6; i++){
-                color += "f"
-            }
-        }
-
-        ctx.scale(5,5);
-        
-        ctx.fillStyle = `#${color}`;
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-        setUri(canvas.toDataURL("image/png", 1));    }
-    ,[props.store.code]);
-
-    const handleKey = (evt:any):void => {
-        if(evt.target.value.length >= 6 && (evt.keyCode !== 8 && evt.keyCode !== 9) && evt.keyCode !== 17 && evt.keyCode !== 65
-            && evt.keyCode !== 36 && evt.keyCode !== 37 && evt.keyCode !== 38 && evt.keyCode !== 39) evt.target.blur();
-    }
-
-    const handleCode = (evt:any) => {
-        if(evt.target.value.length > 6 ) return false;
-        dispatch({
-            type: "updateCode",
-            code: evt.target.value
-        });
-
+    const generate = () => {
         if(props.store.code === undefined) return;
 
         let canvas:any = document.getElementById("generate-canvas"),
@@ -106,6 +60,30 @@ const Generate:FunctionComponent<any> = (props:props):ReactElement => {
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
         setUri(canvas.toDataURL("image/png", 1));
+    }
+
+    useEffect(() => {
+        dispatch({
+            type: "updateCode",
+            code: props.match.params.id
+        });
+    },[dispatch, props.match.params.id]);
+    useEffect(() => {
+        generate();
+    },[]);
+
+    const handleKey = (evt:any):void => {
+        if(evt.target.value.length >= 6 && (evt.keyCode !== 8 && evt.keyCode !== 9) && evt.keyCode !== 17 && evt.keyCode !== 65
+            && evt.keyCode !== 36 && evt.keyCode !== 37 && evt.keyCode !== 38 && evt.keyCode !== 39) evt.target.blur();
+    }
+
+    const handleCode = (evt:any) => {
+        if(evt.target.value.length > 6 ) return false;
+        dispatch({
+            type: "updateCode",
+            code: evt.target.value
+        });
+        generate();
     }
 
     const redirect = ():void => {
